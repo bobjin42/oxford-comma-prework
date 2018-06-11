3a3b4862c00fcb10f1f84a648310e4e8cec6328f
Yicheng Jin <bobjin42@gmail.com>
2018-06-11 18:36:32 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,8 @@
 def oxford_comma(array)
   if array.length < 2;
     puts array.join(" ")
+  elsif array.length == 2;
+    puts "#{array[0]} and #{array[1]}" 
   else 
     last_ele = array[-1]
     new_last_ele = ", and #{last_ele}"
